bfb1f53370477bb4e2ea498f2f4e4f20dc1d67cd
hvoyvng <jyu0628@gmail.com>
2022-01-24 12:12:00 -0800

    fixed notes html

---
+++ routes/itemRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router()
+const path = require('path')
+const fs = require('fs')
+
+//function getting notes
+router.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+        res.json(JSON.parse(data))
+    })
+})
+
+//function posting notes to JSON file
+router.post('/api/notes', (req, res) => {
+    const note = req.body
+    fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+        const newNotes = JSON.parse(data)
+        newNotes.push(note)
+        fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(newNotes), err => {
+            if (err) { console.log(err) }
+            res.sendStatus(200)
+        })
+    })
+})
+
+//function for deleting notes
+router.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        const deletedNotes = JSON.parse(data)
+        for (let i = 0; i < deletedNotes.length; i++) {
+            const dbElement = deletedNotes[i]
+            if (dbElement.id === req.params.id) {
+                deletedNotes.splice(i, 1)
+            }
+        }
+        fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
+            if (err) { console.log(err) }
+            res.sendStatus(200)
+        })
+    })
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const router = require('express').Router()
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./routes/itemRoutes.js'))
+
+//sends notes file to notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+
+//sends index file to *
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//port for heroku
+const port = process.env.PORT || 4000;
+
+app.listen(port);
